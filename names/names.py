a1d0606bdc744ac095a951f272ac5897e4bdd7d0
Michael Mangus <mjmangus16@yahoo.com>
2019-12-22 11:22:13 -0500

    final commit MVP completed

--- names/names.py
+++ names/names.py
@@ -1,26 +1,74 @@
 import time
 
+
+def quicksort(items):
+    if len(items) <= 1:
+        return items
+    # 1. Select the last element as the pivot.
+    pivot = items[-1]
+    left = []
+    right = []
+    for i in range(len(items) - 1):
+        item = items[i]
+        if item < pivot:
+            # 2. Move all elements smaller than the pivot to the left.
+            left.append(item)
+        else:
+            # 3. Move all elements greater than the pivot to the right.
+            right.append(item)
+    #print(f"LEFT: {left}, PIVOT: {pivot}, RIGHT: {right}")
+    # 4. While LHS and RHS are greater than 1, repeat steps 1-3 on each side.
+    return quicksort(left) + [pivot] + quicksort(right)
+
+
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+def find_dups(list_1, list_2):
+    i = j = 0
+    duplicates = []
+
+    while i < len(list_1) and j < len(list_2):
+        if list_1[i] > list_2[j]:
+            j += 1
+        elif list_1[i] < list_2[j]:
+            i += 1
+        elif list_1[i] == list_2[j]:
+            duplicates.append(list_1[i])
+            i += 1
+            j += 1
+
+    return duplicates
+
+#   I  [1,2,3,4,5,6,7]
+
+#   J  [4,5,6,7,8,9,10]
+
+
+duplicates = find_dups(quicksort(names_1), quicksort(names_2))
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish with no restrictions on techniques or data
 # structures?
+
+# cont = {}
+# duplicates = []
+# for name in names_1:
+#     cont.update({name: name})
+
+# for name in names_2:
+#     if name in cont:
+#         duplicates.append(name)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,4 +1,6 @@
 from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
 
 
 class RingBuffer:
@@ -8,13 +10,29 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        print("2", self.current, self.storage.tail)
+        if len(self.storage) >= self.capacity:
+            print(self.current, self.storage.tail)
+            if self.current is self.storage.tail:
+                self.current = self.storage.head
+                self.storage.head.value = item
+            else:
+                self.current.next.value = item
+                self.current = self.current.next
+
+        else:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        curr = self.storage.head
+        for i in range(0, self.storage.length):
+            list_buffer_contents.append(curr.value)
+            curr = curr.next
 
         return list_buffer_contents
 
@@ -30,3 +48,21 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+buffer = RingBuffer(3)
+print(buffer.get())
+buffer.append('7')
+buffer.append('6')
+buffer.append('5')
+print(buffer.get())
+buffer.append('4')
+print(buffer.get())
+buffer.append('3')
+print(buffer.get())
+buffer.append('2')
+print(buffer.get())
+
+print(buffer.storage.head.value)
+print(buffer.storage.head.next.value)
+print(buffer.storage.head.next.next.value)
